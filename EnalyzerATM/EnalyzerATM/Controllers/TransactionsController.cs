500b40d68c5beeceffdd0df3ea10bdd2863da6d1
Eva Kaštelan <eva.kastelan@gmail.com>
2023-09-27 13:03:41 +0200

    Add TransactionsController and InputForm and Deposit Views

---
+++ EnalyzerATM/EnalyzerATM/Controllers/TransactionsController.cs
@@ -0,0 +1,34 @@
+﻿using EnalyzerATM.Data;
+using EnalyzerATM.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnalyzerATM.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> InputForm()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Deposit(int amount)
+        {
+
+            TransactionCalculator calculator = new TransactionCalculator(_context);
+            ATMDeposit atmDeposit = calculator.call(amount);
+
+            return View(atmDeposit);
+            //return View(amount);
+        }
+
+    }
+}
---
+++ EnalyzerATM/EnalyzerATM/wwwroot/js/InputForm.js
@@ -0,0 +1,32 @@
+﻿
+function handleButtonClick(value) {
+        const inputValue = document.getElementById("inputValue");
+        const hiddenInput = document.getElementById("hiddenInput");
+        inputValue.value += value;
+        hiddenInput.value = inputValue.value; 
+        checkInput()
+}
+
+function handleBackspaceClick() {
+        const inputValue = document.getElementById("inputValue");
+        const hiddenInput = document.getElementById("hiddenInput");
+        inputValue.value = inputValue.value.slice(0, -1);
+        hiddenInput.value = inputValue.value;
+        checkInput()
+}
+
+function handleSubmit() {
+        const form = document.getElementById("numpadForm");
+    form.submit();
+}
+
+function checkInput() {
+    var hiddenInput = document.getElementById('hiddenInput');
+    var submitButton = document.getElementById('submitButton');
+
+    if (hiddenInput.value.trim() !== '') {
+        submitButton.removeAttribute('disabled');
+    } else {
+        submitButton.setAttribute('disabled', 'disabled');
+    }
+}
