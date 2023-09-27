5335140835af876c8bd7a897cdaed477d9e06a92
Eva Kaštelan <eva.kastelan@gmail.com>
2023-09-27 18:25:43 +0200

    Add unit tests for business logic

--- EnalyzerATM/EnalyzerATM/Data/ApplicationDbContext.cs
+++ EnalyzerATM/EnalyzerATM/Data/ApplicationDbContext.cs
@@ -1,23 +1,26 @@
 ﻿namespace EnalyzerATM.Data
 {
+    using Effort;
     using EnalyzerATM.Models;
     using Microsoft.EntityFrameworkCore;
+    using System.Data.Common;
+
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
-
+        
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlite("Data Source=..\\ATM.db;");
         }
 
-        public DbSet<Accounts> Accounts { get; set; }
-        public DbSet<Coins> Coins { get; set; }
-        public DbSet<Notes> Notes { get; set; }
-        public DbSet<Transactions> Transactions { get; set; }
+        public virtual DbSet<Accounts> Accounts { get; set; }
+        public virtual DbSet<Coins> Coins { get; set; }
+        public virtual DbSet<Notes> Notes { get; set; }
+        public virtual DbSet<Transactions> Transactions { get; set; }
 
     }
 
