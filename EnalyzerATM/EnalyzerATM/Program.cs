1090f05d7132ff626753141546ddcd9d69d7aa18
Eva Kaštelan <eva.kastelan@gmail.com>
2023-09-27 15:39:33 +0200

    Remove footer and rerout Home to InputForm View

--- EnalyzerATM/EnalyzerATM/Program.cs
+++ EnalyzerATM/EnalyzerATM/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Transactions}/{action=InputForm}/{id?}");
 
 app.Run();
