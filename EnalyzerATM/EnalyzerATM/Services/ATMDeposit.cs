1b6d3132681be283095d4e994eccd79f5a500d60
Eva Kaštelan <eva.kastelan@gmail.com>
2023-09-27 12:26:24 +0200

    Add ATM logic

---
+++ EnalyzerATM/EnalyzerATM/Services/ATMDeposit.cs
@@ -0,0 +1,66 @@
+﻿using EnalyzerATM.Models;
+
+namespace EnalyzerATM.Services
+{
+    public class ATMDeposit
+    {
+        public ATMDeposit()
+        {
+            this.currentAmount = 0;
+        }
+
+        private int currentAmount;
+        private Dictionary<int, int> notes = new Dictionary<int, int>();
+        private Dictionary<int, int> smallCoins = new Dictionary<int, int>();
+        private Dictionary<int, int> bigCoins = new Dictionary<int, int>();
+
+        public void addNotes(int value, int count)
+        {
+            if (count == 0)
+                return;
+
+            notes.Add(value, count);
+            setCurrentAmount(currentAmount + count * value);
+        }
+        public void addBigCoins(int value, int count)
+        {
+            if (count == 0)
+                return;
+
+            bigCoins.Add(value, count);
+            setCurrentAmount(currentAmount + count * value);
+        }
+        public void addSmallCoins(int value, int count)
+        {
+            if (count == 0)
+                return;
+
+            smallCoins.Add(value, count);
+            setCurrentAmount(currentAmount + count * value);
+        }
+        public int getCurrentAmount()
+        {
+            return currentAmount;
+        }
+
+        public void setCurrentAmount(int value)
+        {
+            this.currentAmount = value;
+        }
+
+        public Dictionary<int, int> getNotes()
+        {
+            return notes;
+        }
+
+        public Dictionary<int, int> getBigCoins()
+        {
+            return bigCoins;
+        }
+
+        public Dictionary<int, int> getSmallCoins()
+        {
+            return smallCoins;
+        }
+    }
+}
---
+++ EnalyzerATM/EnalyzerATM/Services/TransactionCalculator.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using EnalyzerATM.Data;
+using EnalyzerATM.Models;
+using EnalyzerATM.Services;
+
+namespace EnalyzerATM.Services
+{
+    public class TransactionCalculator
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionCalculator(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ATMDeposit call(int amount)
+        {
+            ATMDeposit atmDeposit = new ATMDeposit();
+
+            calculateBankNotes(amount, atmDeposit);
+            calculateBigCoins(amount - atmDeposit.getCurrentAmount(), atmDeposit);
+            calculateSmallCoins(amount - atmDeposit.getCurrentAmount(), atmDeposit);
+
+            return atmDeposit;
+        }
+
+        private void calculateBankNotes(int amount, ATMDeposit atmDeposit)
+        {
+            List<Notes> notes = _context.Notes.Where(note => note.Count > 0)
+                                              .OrderByDescending(note => note.Value)
+                                              .ToList();
+            foreach (Notes note in notes)
+            {
+                int division = amount / note.Value;
+                int count = division <= note.Count ? division : note.Count;
+                note.Count -= count;
+                amount -= note.Value * count;
+
+                atmDeposit.addNotes(note.Value, count);
+            }
+
+            _context.SaveChanges();
+
+        }
+
+        private void calculateBigCoins(int amount, ATMDeposit atmDeposit)
+        {
+            List<Coins> coins = _context.Coins.Where(coin => coin.Count > 0)
+                                              .Where(coin => coin.Diameter > 20)
+                                              .OrderByDescending(coin => coin.Value)
+                                              .ToList();
+            foreach (Coins coin in coins)
+            {
+                int division = amount / coin.Value;
+                int count = division <= coin.Count ? division : coin.Count;
+                coin.Count -= count;
+                amount -= coin.Value * count;
+
+                atmDeposit.addBigCoins(coin.Value, count);
+            }
+
+            _context.SaveChanges();
+        }
+
+        private void calculateSmallCoins(int amount, ATMDeposit atmDeposit)
+        {
+            List<Coins> coins = _context.Coins.Where(coin => coin.Count > 0)
+                                              .Where(coin => coin.Diameter <= 20)
+                                              .OrderByDescending(coin => coin.Value)
+                                              .ToList();
+            foreach (Coins coin in coins)
+            {
+                int division = amount / coin.Value;
+                int count = division <= coin.Count ? division : coin.Count;
+                coin.Count -= count;
+                amount -= coin.Value * count;
+
+                atmDeposit.addSmallCoins(coin.Value, count);
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
