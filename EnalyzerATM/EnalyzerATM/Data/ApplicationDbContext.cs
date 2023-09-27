165ca94d3c5f91dd1da8fc499c7e81fb6d5d81ed
Eva Kaštelan <eva.kastelan@gmail.com>
2023-09-27 10:49:34 +0200

    Add dbconnection and install packages

---
+++ EnalyzerATM/EnalyzerATM/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace ATMapp.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Transactions;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=..\\ATM.db;");
+        }
+
+
+    }
+
+}
